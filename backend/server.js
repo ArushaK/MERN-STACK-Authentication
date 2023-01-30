4eaa6b8d50a0684aa425b7d01ef7b5a93466c32a
ArushaK <arusha.kamate007@gmail.com>
2023-01-30 14:46:30 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json()); //for raw data
+app.use(express.urlencoded({ extended: false })); //for urlencoded
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
