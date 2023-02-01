f5c78bc3c9bf6eefef2c4af30193998ba507a002
ArushaK <arusha.kamate007@gmail.com>
2023-02-01 19:01:31 +0530

    Authentication complete

---
+++ frontend/src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="loadiingSpinnerContainer">
+      <div className="loadingSpinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
